e1052ac3a76cdd095974ba6221e0f3373cc957a0
mattiasback <mattias.j.back@gmail.com>
2019-12-11 20:52:54 +0100

    remove out folder from tracking

--- HelloKotlin/src/AMS.kt
+++ HelloKotlin/src/AMS.kt
@@ -1,3 +1,4 @@
+import java.util.function.Consumer
 import kotlin.random.Random
 
 //Aquarium Management System
@@ -9,6 +10,11 @@ fun main(args: Array<String>)
     {
         feedTheFish()
     }
+
+    println(canAddFish(10.0, listOf(3,3,3)))
+    println(canAddFish(8.0, listOf(2,2,2), hasDecorations = false))
+    println(canAddFish(9.0, listOf(1,1,3), 3))
+    println(canAddFish(10.0, listOf(), 7, true))
 }
 
 fun feedTheFish()
@@ -35,4 +41,24 @@ fun fishFood(day: String) : String
     }
 
     return food
+}
+
+fun canAddFish(gallons: Double, currentFish: List<Int>, fishSize: Int = 2, hasDecorations: Boolean = true) : Boolean
+{
+    var canFitFish = false
+
+    var inchPerGallonRatio: Double = if(hasDecorations) 0.8 else 1.0
+
+    var currentFishLength = 0
+    for(i in currentFish)
+    {
+        currentFishLength += i
+    }
+
+    if((currentFishLength + fishSize) <= (gallons * inchPerGallonRatio))
+    {
+        canFitFish = true
+    }
+
+    return canFitFish
 }
