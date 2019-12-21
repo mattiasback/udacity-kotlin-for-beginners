8deaf649af2e2269c3ef4ccd41c5b7fd9df1afcd
mattiasback <mattias.j.back@gmail.com>
2019-12-21 12:12:06 +0100

    Filter example update

--- HelloKotlin/src/FilterExample.kt
+++ HelloKotlin/src/FilterExample.kt
@@ -9,6 +9,7 @@ fun main(args: Array<String>)
     println(startWithCEndWithE)
     
     println(getFirstThreeStartsWithC(spices))
+    println(getFirstThreeStartsWithC2(spices))
 }
 
 fun getCurries(spices: List<String>) : List<String>
@@ -23,5 +24,11 @@ fun getStartWithCEndWithE(spices: List<String>) : List<String>
 
 fun getFirstThreeStartsWithC(spices: List<String>) : List<String>
 {
-    return spices.subList(0, 2).filter { s -> s.startsWith('c')}
+    //End index is exclusive in sublist
+    return spices.subList(0, 3).filter { s -> s.startsWith('c')}
+}
+
+fun getFirstThreeStartsWithC2(spices: List<String>) : List<String>
+{
+    return spices.take(3).filter { s -> s.startsWith('c') }
 }
