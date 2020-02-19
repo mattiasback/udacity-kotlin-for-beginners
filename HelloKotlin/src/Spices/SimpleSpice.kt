b3915812c4724ff8702088f410a66be1e1813fa5
mattiasback <mattias.j.back@gmail.com>
2020-02-19 20:13:08 +0100

    Add Spice class

--- HelloKotlin/src/Spices/SimpleSpice.kt
+++ HelloKotlin/src/Spices/SimpleSpice.kt
@@ -1,6 +1,35 @@
 package Spices
 
-public class SimpleSpice()
+fun main(args: Array<String>)
+{
+    var spice = SimpleSpice()
+    println(spice.name + " has a heat of " + spice.heat)
+
+    val spices = listOf(
+        Spice("curry") ,
+        Spice("pepper"),
+        Spice("cayenne", "hot"),
+        Spice("ginger"),
+        Spice("red curry", "hot"),
+        Spice("green curry", "hot"),
+        Spice("red pepper"),
+        makeSalt())
+
+    val spicy = spices.filter { s -> s.heat > 5 }
+    val lessSpicy = spices.filter { s -> s.heat <= 5 }
+
+    for (spice in spicy)
+    {
+        println("SPICY " + spice.stringRepresentation())
+    }
+    for (spice in lessSpicy)
+    {
+        println("LESS SPICY " + spice.stringRepresentation())
+    }
+}
+
+
+class SimpleSpice()
 {
     val name: String = "curry"
     val spiciness: String = "mild"
@@ -9,8 +38,27 @@ public class SimpleSpice()
         get() = 5
 }
 
-fun main(args: Array<String>)
+class Spice(val name: String, val spiciness: String = "mild")
 {
-    var spice = SimpleSpice()
-    println(spice.name + " has a heat of " + spice.heat)
+    val heat: Int
+        get()
+        {
+            return when (spiciness.toLowerCase()) {
+                "mild" -> 5
+                "hot" -> 10
+                else -> 0
+            }
+        }
+
+    init {
+        println(stringRepresentation())
+    }
+
+    fun stringRepresentation() : String
+    {
+        return "${this.name} ${this.spiciness} ${this.heat}"
+    }
 }
+
+fun makeSalt() = Spice("Salt")
+
