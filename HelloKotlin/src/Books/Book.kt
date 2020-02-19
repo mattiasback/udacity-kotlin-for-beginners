6d800945a5a3c5a7765c2061e6186d29827e3512
mattiasback <mattias.j.back@gmail.com>
2020-02-19 23:05:54 +0100

    Pairs and Triples

--- HelloKotlin/src/Books/Book.kt
+++ HelloKotlin/src/Books/Book.kt
@@ -2,7 +2,7 @@ package books
 
 fun main(args: Array<String>)
 {
-    val book = Book()
+    val book = Book("Moby Dick", "Charles Dickens")
     println(book.readPage())
     println(book.readPage())
     println(book.readPage())
@@ -11,19 +11,36 @@ fun main(args: Array<String>)
     println(eBook.readPage())
     println(eBook.readPage())
     println(eBook.readPage())
+
+    val (title, author) = book.getTitleAndAuthor()
+    val (title2, author2, year) = book.getTitleAndAuthorAndYear()
+
+    println("${title} ${author}")
+    println("${title2} ${author2} ${year}")
 }
 
-open class Book {
-
+open class Book(val title: String = "Oliver Twist", val author: String = "Charles Dickens", val year: Int = 1839)
+{
     private var currentPage = 1
 
     open fun readPage(): Int {
         currentPage++
         return currentPage
     }
+
+    fun getTitleAndAuthor(): Pair<String, String>
+    {
+        return Pair(title, author)
+    }
+
+    fun getTitleAndAuthorAndYear(): Triple<String, String, Int>
+    {
+        return Triple(title, author, year)
+    }
 }
 
-class eBook(val format: String = "text") : Book() {
+class eBook(val format: String = "text") : Book()
+{
 
     private var wordsRead = 0
 
