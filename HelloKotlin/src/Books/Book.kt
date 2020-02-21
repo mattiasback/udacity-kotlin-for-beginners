de2e069e7d3c382a1f7a50d37513956794916299
mattiasback <mattias.j.back@gmail.com>
2020-02-21 20:29:35 +0100

    Extensions

--- HelloKotlin/src/Books/Book.kt
+++ HelloKotlin/src/Books/Book.kt
@@ -1,5 +1,12 @@
 package books
 
+const val MAXIMUM_NUMBER_OF_BOOKS = 10
+
+object Constants
+{
+    val BASE_URL = "library/books/"
+}
+
 fun main(args: Array<String>)
 {
     val book = Book("Moby Dick", "Charles Dickens")
@@ -17,9 +24,29 @@ fun main(args: Array<String>)
 
     println("${title} ${author}")
     println("${title2} ${author2} ${year}")
+
+    //Author Charles Dickens
+    val allBooks = setOf(
+        Book("Oliver Twist", year = 1839),
+        Book("David Copperfield", year = 1850),
+        Book("Bleak House", year = 1853)
+    )
+
+    var library = mapOf("Charles Dickens" to allBooks)
+
+    val isOliverTwistReturned = library.any { author -> author.value.any { book -> book.title == "Oliver Twist" } }
+    println("Oliver Twist is returned: $isOliverTwistReturned")
+
+    var moreBooks = mutableMapOf("Shakespeare" to "Hamlet")
+    moreBooks.getOrPut("Shakespeare") { "Hamlet" }
+    moreBooks.put("Shakespeare", "Romeo and Juliet")
+
+    println(moreBooks)
+
+    book.printUrl()
 }
 
-open class Book(val title: String = "Oliver Twist", val author: String = "Charles Dickens", val year: Int = 1839)
+open class Book(val title: String = "Oliver Twist", val author: String = "Charles Dickens", val year: Int = 1839, var pages: Int = 500)
 {
     private var currentPage = 1
 
@@ -37,15 +64,37 @@ open class Book(val title: String = "Oliver Twist", val author: String = "Charle
     {
         return Triple(title, author, year)
     }
+
+    fun canBorrow(numBorrowedBooks: Int): Boolean
+    {
+        return numBorrowedBooks < MAXIMUM_NUMBER_OF_BOOKS
+    }
+
+    fun printUrl()
+    {
+        println("${BASE_URL}${title.filter { c -> c != ' ' }}.html")
+    }
+
+    companion object Constants
+    {
+        const val BASE_URL = "library/books/"
+    }
 }
 
 class eBook(val format: String = "text") : Book()
 {
-
     private var wordsRead = 0
 
     override fun readPage(): Int {
         wordsRead += 250
         return wordsRead
     }
+}
+
+//2.1 grams per page
+fun Book.weight(): Double = pages*2.1
+
+fun Book.tornPages(torn: Int): Int {
+    pages -= torn
+    return pages
 }
