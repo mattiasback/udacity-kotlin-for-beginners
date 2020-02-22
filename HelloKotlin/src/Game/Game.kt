0141248e7d5ec12d2e0ef3b4738c43c77e9ba34f
mattiasback <mattias.j.back@gmail.com>
2020-02-22 11:32:46 +0100

    bug fix

--- HelloKotlin/src/Game/Game.kt
+++ HelloKotlin/src/Game/Game.kt
@@ -1,14 +1,14 @@
-package Game
+package game
 
 fun main(args: Array<String>)
 {
     val game = Game()
     println(game.path)
-    game.north
-    game.south
-    game.east
-    game.west
-    game.end
+    game.north()
+    game.south()
+    game.east()
+    game.west()
+    game.end()
     println(game.path)
 }
 
@@ -25,10 +25,10 @@ class Game
 {
     val path = mutableListOf(Directions.START)
 
-    val north = path.add(Directions.NORTH)
-    val south = path.add(Directions.SOUTH)
-    val west = path.add(Directions.WEST)
-    val east = path.add(Directions.EAST)
+    val north = { path.add(Directions.NORTH) }
+    val south = { path.add(Directions.SOUTH) }
+    val west = { path.add(Directions.WEST) }
+    val east = { path.add(Directions.EAST) }
     val end = {
         path.add(Directions.END)
         println("GAME OVER")
