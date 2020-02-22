a04a33a5088969704bb589b2134b537ef0af2f04
mattiasback <mattias.j.back@gmail.com>
2020-02-22 09:41:16 +0100

    Generic functions

--- HelloKotlin/src/Buildings/Buildings.kt
+++ HelloKotlin/src/Buildings/Buildings.kt
@@ -1,10 +1,15 @@
 package Buildings
 
 
-fun main(args: Array<String>)
+fun main()
 {
-    Building(Wood()).build()
-    Building(Brick()).build()
+    var woodenBuilding = Building(Wood())
+    woodenBuilding.build()
+    var brickBuilding = Building(Brick())
+    brickBuilding.build()
+
+    isSmallBuilding(woodenBuilding)
+    isSmallBuilding(brickBuilding)
 }
 
 open class BaseBuildingMaterial(val numberNeeded: Int = 1)
@@ -12,14 +17,20 @@ open class BaseBuildingMaterial(val numberNeeded: Int = 1)
 class Wood() : BaseBuildingMaterial(numberNeeded = 4)
 class Brick() : BaseBuildingMaterial(numberNeeded = 8)
 
-class Building<T: BaseBuildingMaterial>(var material: T)
+class Building<out T: BaseBuildingMaterial>(val material: T)
 {
-    val baseMaterialsNeeded = 100
+    private val baseMaterialsNeeded = 100
     fun actualMaterialsNeeded() = material.numberNeeded * baseMaterialsNeeded
 
     fun build()
     {
         val typeName = material::class.simpleName
-        println("Building requires ${actualMaterialsNeeded()} ${typeName}")
+        println("Building requires ${actualMaterialsNeeded()} $typeName")
     }
+}
+
+fun <T : BaseBuildingMaterial> isSmallBuilding(building: Building<T>)
+{
+    if (building.actualMaterialsNeeded() < 500) println("Small building")
+    else println("Large building")
 }
