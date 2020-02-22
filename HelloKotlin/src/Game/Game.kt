7391d347fb6a698f17b6621092d2199f317a3f6b
mattiasback <mattias.j.back@gmail.com>
2020-02-22 11:09:43 +0100

    Add Game class

---
+++ HelloKotlin/src/Game/Game.kt
@@ -0,0 +1,39 @@
+package Game
+
+fun main(args: Array<String>)
+{
+    val game = Game()
+    println(game.path)
+    game.north
+    game.south
+    game.east
+    game.west
+    game.end
+    println(game.path)
+}
+
+enum class Directions{
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+    START,
+    END
+}
+
+class Game
+{
+    val path = mutableListOf(Directions.START)
+
+    val north = path.add(Directions.NORTH)
+    val south = path.add(Directions.SOUTH)
+    val west = path.add(Directions.WEST)
+    val east = path.add(Directions.EAST)
+    val end = {
+        path.add(Directions.END)
+        println("GAME OVER")
+        println(path)
+        path.clear()
+        false
+    }
+}
